0e7b25a3699fe40083925e982b55f48338b81140
josiarod <josiarodriguez@gmail.com>
2020-01-22 12:48:04 -0500

    Hello World Exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is January 22, 2020. I am alive!");
+        System.out.println("My name is Josia.");
+    }
+}
